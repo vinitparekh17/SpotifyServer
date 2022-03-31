9d83fe9bb39e038d4b5b8754f211dfca9c8ed128
Vinit Parekh <vinitrevaliya@gmail.com>
2022-03-31 15:43:22 +0530

    Spoify Server

---
+++ server.js
@@ -0,0 +1,64 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder")
+const SpotifyWebApi = require("spotify-web-api-node")
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebApi({
+    clientId: `dea12af6833d4125ae9ecb7aa8d89144`,
+    clientSecret: `b21a1ada01d94f6780023519109c5ae9`,
+    redirectUri: `http://localhost:3000`,
+    refreshToken: refreshToken
+  })
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    clientId: `dea12af6833d4125ae9ecb7aa8d89144`,
+    clientSecret: `b21a1ada01d94f6780023519109c5ae9`,
+    redirectUri: `http://localhost:3000`,
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      res.sendStatus(400)
+    })
+})
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+  res.json({ lyrics })
+})
+
+app.listen(3001)
